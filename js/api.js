57ea42ea5ef6025a50cdea81331e75ce55f0637a
Dakota M <dakota.paige@hotmail.com>
2018-03-04 10:00:20 -0800

    Added JS to submit a quote to the posts

--- js/api.js
+++ js/api.js
@@ -1,8 +1,10 @@
 (function($) {
 
+  var lastPage = '';
   //fetch a random quote post when the new quote button is clicked
   $('#new-quote-button').on('click', function(event) {
     event.preventDefault();
+    lastPage = document.URL;
 
     $.ajax( {
       url: '/QuotesOnDev/wp-json/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
@@ -27,14 +29,52 @@
         }
 
         //history api to push post on the end of the url slug
-        var stateObj = '';
-        history.replaceState(stateObj, 'post page', post.link);
+
+        history.pushState(null, null, post.link);
+
       }
     })
+    
   })
 
 
+  $(window).on('popstate', function() {
+    if (window.location.hash.indexOf('qm-overview') === 1) {
+      return false;
+    }else {
+      window.location.replace(lastPage);
+    }
+  });
+
   //submit a new quote with the form using ajax
+
+  $('input[type=submit]').on('click', function(event) {
+    event.preventDefault();
+    var data = {
+      status: 'pending',
+      title: $('#quote-author').val(),
+      content: $('#quote-content').val(),
+      _qod_quote_source: $('#quote-source').val(),
+      _qod_quote_source_url: $('#quote-source-url').val()
+    }
+
+    $.ajax({
+       method: 'post',
+       url: api_vars.root_url + 'wp/v2/posts/',
+       data: data,
+       beforeSend: function(xhr) {
+          xhr.setRequestHeader( 'X-WP-Nonce', api_vars.nonce );
+       },
+       success: function() {
+          $('#quote-submission-form').hide('slow');
+          $('.entry-title').after('<p>'+api_vars.success+'</p>');
+       },
+       error: function() {
+        $('#quote-submission-form').hide('slow');
+        $('.entry-title').after('<p>'+api_vars.failure+'</p>');
+       }
+    });
+ });
 
 
 })(jQuery);
